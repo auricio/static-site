8b343aff7ed630117def35783df518a691eabe98
Auricio Santos Leite <3136409+auricio@users.noreply.github.com>
2025-05-08 15:01:01 -0300

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,14 +4,14 @@ echo "codespace ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/codespace-nopa
 sudo chmod 440 /etc/sudoers.d/codespace-nopasswd
 
 # Instala PostgreSQL Client (para psql)
-sudo apt-get update && sudo apt-get install -y postgresql-client
+sudo apt update #&& sudo apt-get install -y postgresql-client
 
 # Configura PostgreSQL
-sudo -u postgres psql <<-EOSQL
-  CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';
-  CREATE DATABASE ${POSTGRES_DB};
-  GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
-EOSQL
+#sudo -u postgres psql <<-EOSQL
+#  CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';
+#  CREATE DATABASE ${POSTGRES_DB};
+#  GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
+#EOSQL
 
 # Configura Python (Django)
 python -m venv /home/codespace/.venv
